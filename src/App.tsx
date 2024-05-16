66974182c03d142753c7da4a412f6a9d86ba3b11
Iftah Saar <iftah.saar@abra-it.com>
2024-05-16 18:51:51 +0300

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,52 @@
-import React, { FunctionComponent, useState, useEffect, useMemo } from 'react';
-import axios from 'axios';
+import React, { FunctionComponent, useState, useEffect, useMemo } from "react";
+import axios from "axios";
 
 interface IProps {}
 
-type status = 'WORKING' | 'NOT WORKING' | 'LOADING'
+type status = "WORKING" | "NOT WORKING" | "LOADING";
 
 const statusInfo = {
-  'WORKING': {
-    statusMessage: 'Fake server is working',
-    statusColor: 'green',
-  },
-  'NOT WORKING': {
-    statusMessage: 'Fake server is not working',
-    statusColor: 'red',
-  },
-  'LOADING': {
-    statusMessage: 'Loading...',
-    statusColor: 'yellow',
-  },
+    WORKING: {
+        statusMessage: "Fake server is working",
+        statusColor: "green",
+    },
+    "NOT WORKING": {
+        statusMessage: "Fake server is not working",
+        statusColor: "red",
+    },
+    LOADING: {
+        statusMessage: "Loading...",
+        statusColor: "yellow",
+    },
 };
 
 const App: FunctionComponent<IProps> = () => {
-  const [serverStatus, setServerStatus] = useState<status>('LOADING');
+    const [serverStatus, setServerStatus] = useState<status>("LOADING");
 
-  const { statusMessage, statusColor } = useMemo(() => statusInfo[serverStatus], [serverStatus]);
+    const { statusMessage, statusColor } = useMemo(
+        () => statusInfo[serverStatus],
+        [serverStatus]
+    );
 
-  useEffect(() => {
-    const fakeServerUrl = 'http://localhost:4000/databases';
+    useEffect(() => {
+        const fakeServerUrl = "http://localhost:4000/databases";
 
-    axios.get(fakeServerUrl)
-      .then((response) => {
-        setServerStatus('WORKING');
-        console.log(response.data);
-      })
-      .catch(() => {
-        setServerStatus('NOT WORKING');
-      });
-  }, []);
+        axios
+            .get(fakeServerUrl)
+            .then((response) => {
+                setServerStatus("WORKING");
+                console.log(response.data);
+            })
+            .catch(() => {
+                setServerStatus("NOT WORKING");
+            });
+    }, []);
 
-  return (
-    <div>
-      <h5 style={{ color: statusColor }}>
-        {statusMessage}
-      </h5>
-    </div>
-  );
-}
+    return (
+        <div>
+            <h5 style={{ color: statusColor }}>{statusMessage}</h5>
+        </div>
+    );
+};
 
 export default App;
